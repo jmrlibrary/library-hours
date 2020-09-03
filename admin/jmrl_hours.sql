bd2682ff56642f38d58fe7818bd025483854a6c3
Katherine Perdue <kperdue@jmrl.org>
2020-09-03 14:02:29 -0400

    Added .gitignore and sample config file and updated sample sql file

--- admin/jmrl_hours.sql
+++ admin/jmrl_hours.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 13, 2020 at 01:52 PM
--- Server version: 5.7.30
+-- Generation Time: Sep 03, 2020 at 01:38 PM
+-- Server version: 5.7.31
 -- PHP Version: 7.3.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -21,29 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `jmrl_hours`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Alert`
---
-
-CREATE TABLE `Alert` (
-  `LibID` int(11) NOT NULL,
-  `StartDate` date NOT NULL,
-  `EndDate` date NOT NULL,
-  `Message` varchar(300) NOT NULL,
-  `AlertID` smallint(6) NOT NULL,
-  `SameForAll` tinyint(1) NOT NULL,
-  `Name` varchar(60) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `Alert`
---
-
-INSERT INTO `Alert` (`LibID`, `StartDate`, `EndDate`, `Message`, `AlertID`, `SameForAll`, `Name`) VALUES
-(6, '2020-02-29', '2020-03-14', 'Nelson Library is closed for renovations February 29th - March 14th.', 1, 1, 'Nelson Renovation');
 
 -- --------------------------------------------------------
 
@@ -291,7 +268,63 @@ INSERT INTO `Schedule` (`SchedID`, `LibID`, `SemID`, `Day`, `OpenTime`, `CloseTi
 (263, 8, 5, 3, '09:00:00', '17:00:00', 0, 0, 0),
 (264, 8, 5, 4, '09:00:00', '21:00:00', 0, 0, 0),
 (265, 8, 5, 5, '09:00:00', '17:00:00', 0, 0, 0),
-(266, 8, 5, 6, '09:00:00', '17:00:00', 0, 0, 0);
+(266, 8, 5, 6, '09:00:00', '17:00:00', 0, 0, 0),
+(267, 1, 6, 0, '13:00:00', '17:00:00', 1, 0, 0),
+(268, 1, 6, 1, '13:00:00', '19:00:00', 0, 0, 0),
+(269, 1, 6, 2, '10:00:00', '16:00:00', 0, 0, 0),
+(270, 1, 6, 3, '10:00:00', '16:00:00', 0, 0, 0),
+(271, 1, 6, 4, '10:00:00', '16:00:00', 0, 0, 0),
+(272, 1, 6, 5, '10:00:00', '16:00:00', 0, 0, 0),
+(273, 1, 6, 6, '10:00:00', '16:00:00', 0, 0, 0),
+(274, 2, 6, 0, '00:00:00', '00:00:00', 1, 0, 0),
+(275, 2, 6, 1, '13:00:00', '19:00:00', 0, 0, 0),
+(276, 2, 6, 2, '10:00:00', '16:00:00', 0, 0, 0),
+(277, 2, 6, 3, '10:00:00', '16:00:00', 0, 0, 0),
+(278, 2, 6, 4, '10:00:00', '16:00:00', 0, 0, 0),
+(279, 2, 6, 5, '10:00:00', '16:00:00', 0, 0, 0),
+(280, 2, 6, 6, '10:00:00', '16:00:00', 0, 0, 0),
+(281, 3, 6, 0, '00:00:00', '00:00:00', 1, 0, 0),
+(282, 3, 6, 1, '13:00:00', '19:00:00', 0, 0, 0),
+(283, 3, 6, 2, '10:00:00', '16:00:00', 0, 0, 0),
+(284, 3, 6, 3, '10:00:00', '16:00:00', 0, 0, 0),
+(285, 3, 6, 4, '10:00:00', '16:00:00', 0, 0, 0),
+(286, 3, 6, 5, '10:00:00', '16:00:00', 0, 0, 0),
+(287, 3, 6, 6, '10:00:00', '16:00:00', 0, 0, 0),
+(288, 4, 6, 0, '00:00:00', '00:00:00', 1, 0, 0),
+(289, 4, 6, 1, '13:00:00', '19:00:00', 0, 0, 0),
+(290, 4, 6, 2, '10:00:00', '14:00:00', 0, 0, 0),
+(291, 4, 6, 3, '10:00:00', '14:00:00', 0, 0, 0),
+(292, 4, 6, 4, '10:00:00', '14:00:00', 0, 0, 0),
+(293, 4, 6, 5, '10:00:00', '14:00:00', 0, 0, 0),
+(294, 4, 6, 6, '10:00:00', '14:00:00', 0, 0, 0),
+(295, 5, 6, 0, '00:00:00', '00:00:00', 1, 0, 0),
+(296, 5, 6, 1, '13:00:00', '19:00:00', 0, 0, 0),
+(297, 5, 6, 2, '10:00:00', '16:00:00', 0, 0, 0),
+(298, 5, 6, 3, '10:00:00', '16:00:00', 0, 0, 0),
+(299, 5, 6, 4, '10:00:00', '16:00:00', 0, 0, 0),
+(300, 5, 6, 5, '10:00:00', '16:00:00', 0, 0, 0),
+(301, 5, 6, 6, '10:00:00', '16:00:00', 0, 0, 0),
+(302, 6, 6, 0, '00:00:00', '00:00:00', 1, 0, 0),
+(303, 6, 6, 1, '13:00:00', '19:00:00', 0, 0, 0),
+(304, 6, 6, 2, '10:00:00', '16:00:00', 0, 0, 0),
+(305, 6, 6, 3, '10:00:00', '16:00:00', 0, 0, 0),
+(306, 6, 6, 4, '10:00:00', '16:00:00', 0, 0, 0),
+(307, 6, 6, 5, '10:00:00', '16:00:00', 0, 0, 0),
+(308, 6, 6, 6, '10:00:00', '16:00:00', 0, 0, 0),
+(309, 7, 6, 0, '00:00:00', '00:00:00', 1, 0, 0),
+(310, 7, 6, 1, '13:00:00', '19:00:00', 0, 0, 0),
+(311, 7, 6, 2, '10:00:00', '16:00:00', 0, 0, 0),
+(312, 7, 6, 3, '10:00:00', '16:00:00', 0, 0, 0),
+(313, 7, 6, 4, '10:00:00', '16:00:00', 0, 0, 0),
+(314, 7, 6, 5, '10:00:00', '16:00:00', 0, 0, 0),
+(315, 7, 6, 6, '10:00:00', '16:00:00', 0, 0, 0),
+(316, 8, 6, 0, '00:00:00', '00:00:00', 1, 0, 0),
+(317, 8, 6, 1, '13:00:00', '19:00:00', 0, 0, 0),
+(318, 8, 6, 2, '10:00:00', '14:00:00', 0, 0, 0),
+(319, 8, 6, 3, '10:00:00', '14:00:00', 0, 0, 0),
+(320, 8, 6, 4, '10:00:00', '14:00:00', 0, 0, 0),
+(321, 8, 6, 5, '10:00:00', '14:00:00', 0, 0, 0),
+(322, 8, 6, 6, '10:00:00', '14:00:00', 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -310,7 +343,8 @@ CREATE TABLE `Season` (
 
 INSERT INTO `Season` (`SeasonID`, `SeasonName`) VALUES
 (1, 'Winter'),
-(2, 'Summer');
+(2, 'Summer'),
+(3, 'COVID hours');
 
 -- --------------------------------------------------------
 
@@ -333,7 +367,8 @@ CREATE TABLE `Semesters` (
 INSERT INTO `Semesters` (`SemID`, `SemYear`, `SeasonID`, `DayStart`, `DayEnd`) VALUES
 (3, 2018, 1, '2018-09-03', '2019-05-20'),
 (4, 2019, 2, '2019-05-21', '2019-09-01'),
-(5, 2019, 1, '2019-09-02', '2020-05-23');
+(5, 2019, 1, '2019-09-02', '2020-05-23'),
+(6, 2020, 3, '2020-05-26', '2020-12-31');
 
 -- --------------------------------------------------------
 
@@ -358,6 +393,15 @@ CREATE TABLE `Special` (
 --
 
 INSERT INTO `Special` (`SpecialID`, `HolName`, `LibID`, `SpecialDate`, `OpenTime`, `CloseTime`, `Closed`, `Appointment`, `Open24`) VALUES
+(266, 'Labor Day', 9, '2020-09-07', NULL, NULL, 1, 0, 0),
+(265, 'Labor Day', 8, '2020-09-07', NULL, NULL, 1, 0, 0),
+(264, 'Labor Day', 7, '2020-09-07', NULL, NULL, 1, 0, 0),
+(263, 'Labor Day', 6, '2020-09-07', NULL, NULL, 1, 0, 0),
+(262, 'Labor Day', 5, '2020-09-07', NULL, NULL, 1, 0, 0),
+(261, 'Labor Day', 4, '2020-09-07', NULL, NULL, 1, 0, 0),
+(260, 'Labor Day', 3, '2020-09-07', NULL, NULL, 1, 0, 0),
+(259, 'Labor Day', 2, '2020-09-07', NULL, NULL, 1, 0, 0),
+(258, 'Labor Day', 1, '2020-09-07', NULL, NULL, 1, 0, 0),
 (246, 'Renovations', 6, '2020-03-14', NULL, NULL, 1, 0, 0),
 (245, 'Renovations', 6, '2020-03-13', NULL, NULL, 1, 0, 0),
 (244, 'Renovations', 6, '2020-02-13', NULL, NULL, 1, 0, 0),
@@ -379,6 +423,13 @@ INSERT INTO `Special` (`SpecialID`, `HolName`, `LibID`, `SpecialDate`, `OpenTime
 (226, 'inclement weather', 6, '2020-01-07', NULL, NULL, 1, 0, 0),
 (225, 'inclement weather', 4, '2020-01-07', '12:00:00', '19:00:00', 0, 0, 0),
 (224, 'inclement weather', 2, '2020-01-07', NULL, NULL, 1, 0, 0),
+(248, 'Independence Day', 1, '2020-07-04', NULL, NULL, 1, 0, 0),
+(249, 'Independence Day', 2, '2020-07-04', NULL, NULL, 1, 0, 0),
+(250, 'Independence Day', 3, '2020-07-04', NULL, NULL, 1, 0, 0),
+(251, 'Independence Day', 4, '2020-07-04', NULL, NULL, 1, 0, 0),
+(252, 'Independence Day', 5, '2020-07-04', NULL, NULL, 1, 0, 0),
+(253, 'Independence Day', 6, '2020-07-04', NULL, NULL, 1, 0, 0),
+(254, 'Independence Day', 7, '2020-07-04', NULL, NULL, 1, 0, 0),
 (212, 'Power Outage', 2, '2019-07-17', '09:00:00', '16:00:00', 1, 0, 0),
 (213, 'Power outage ', 2, '2019-09-11', '09:00:00', '19:00:00', 0, 0, 0),
 (210, 'New Year\'s Eve', 8, '2019-12-31', '13:00:00', '17:00:00', 0, 0, 0),
@@ -393,7 +444,10 @@ INSERT INTO `Special` (`SpecialID`, `HolName`, `LibID`, `SpecialDate`, `OpenTime
 (203, 'New Year\'s Eve', 1, '2019-12-31', '09:00:00', '17:00:00', 0, 0, 0),
 (204, 'New Year\'s Eve', 2, '2019-12-31', '13:00:00', '17:00:00', 0, 0, 0),
 (205, 'New Year\'s Eve', 3, '2019-12-31', '09:00:00', '17:00:00', 0, 0, 0),
+(255, 'Independence Day', 8, '2020-07-04', NULL, NULL, 1, 0, 0),
+(256, 'Independence Day', 9, '2020-07-04', NULL, NULL, 1, 0, 0),
 (215, 'Power Outage', 6, '2019-11-01', NULL, NULL, 1, 0, 0),
+(257, 'Power outage', 5, '2020-08-07', '10:00:00', '12:00:00', 1, 0, 0),
 (84, 'Independence Day', 3, '2019-07-04', NULL, NULL, 1, 0, 0),
 (83, 'Independence Day', 2, '2019-07-04', NULL, NULL, 1, 0, 0),
 (82, 'Independence Day', 1, '2019-07-04', NULL, NULL, 1, 0, 0),
@@ -491,6 +545,7 @@ INSERT INTO `Special` (`SpecialID`, `HolName`, `LibID`, `SpecialDate`, `OpenTime
 (175, 'Presidents\' Day', 6, '2020-02-17', NULL, NULL, 1, 0, 0),
 (176, 'Presidents\' Day', 7, '2020-02-17', NULL, NULL, 1, 0, 0),
 (177, 'Presidents\' Day', 8, '2020-02-17', NULL, NULL, 1, 0, 0),
+(247, 'Blood Drive', 5, '2020-06-06', NULL, NULL, 1, 0, 0),
 (179, 'Memorial Day', 1, '2020-05-25', NULL, NULL, 1, 0, 0),
 (180, 'Memorial Day', 2, '2020-05-25', NULL, NULL, 1, 0, 0),
 (181, 'Memorial Day', 3, '2020-05-25', NULL, NULL, 1, 0, 0),
@@ -502,17 +557,120 @@ INSERT INTO `Special` (`SpecialID`, `HolName`, `LibID`, `SpecialDate`, `OpenTime
 (198, 'Thanksgiving Eve', 4, '2019-11-27', '10:00:00', '17:00:00', 0, 0, 0),
 (197, 'Thanksgiving Eve', 3, '2019-11-27', '12:00:00', '17:00:00', 0, 0, 0),
 (196, 'Thanksgiving Eve', 2, '2019-11-27', '09:00:00', '17:00:00', 0, 0, 0),
-(195, 'Thanksgiving Eve', 1, '2019-11-27', '09:00:00', '17:00:00', 0, 0, 0);
+(195, 'Thanksgiving Eve', 1, '2019-11-27', '09:00:00', '17:00:00', 0, 0, 0),
+(267, 'Staff Training Day', 1, '2020-10-12', NULL, NULL, 1, 0, 0),
+(268, 'Staff Training Day', 2, '2020-10-12', NULL, NULL, 1, 0, 0),
+(269, 'Staff Training Day', 3, '2020-10-12', NULL, NULL, 1, 0, 0),
+(270, 'Staff Training Day', 4, '2020-10-12', NULL, NULL, 1, 0, 0),
+(271, 'Staff Training Day', 5, '2020-10-12', NULL, NULL, 1, 0, 0),
+(272, 'Staff Training Day', 6, '2020-10-12', NULL, NULL, 1, 0, 0),
+(273, 'Staff Training Day', 7, '2020-10-12', NULL, NULL, 1, 0, 0),
+(274, 'Staff Training Day', 8, '2020-10-12', NULL, NULL, 1, 0, 0),
+(275, 'Staff Training Day', 9, '2020-10-12', NULL, NULL, 1, 0, 0),
+(276, 'Veteran\'s Day', 1, '2020-11-11', NULL, NULL, 1, 0, 0),
+(277, 'Veteran\'s Day', 2, '2020-11-11', NULL, NULL, 1, 0, 0),
+(278, 'Veteran\'s Day', 3, '2020-11-11', NULL, NULL, 1, 0, 0),
+(279, 'Veteran\'s Day', 4, '2020-11-11', NULL, NULL, 1, 0, 0),
+(280, 'Veteran\'s Day', 5, '2020-11-11', NULL, NULL, 1, 0, 0),
+(281, 'Veteran\'s Day', 6, '2020-11-11', NULL, NULL, 1, 0, 0),
+(282, 'Veteran\'s Day', 7, '2020-11-11', NULL, NULL, 1, 0, 0),
+(283, 'Veteran\'s Day', 8, '2020-11-11', NULL, NULL, 1, 0, 0),
+(284, 'Veteran\'s Day', 9, '2020-11-11', NULL, NULL, 1, 0, 0),
+(285, 'Thanksgiving', 1, '2020-11-26', NULL, NULL, 1, 0, 0),
+(286, 'Thanksgiving', 2, '2020-11-26', NULL, NULL, 1, 0, 0),
+(287, 'Thanksgiving', 3, '2020-11-26', NULL, NULL, 1, 0, 0),
+(288, 'Thanksgiving', 4, '2020-11-26', NULL, NULL, 1, 0, 0),
+(289, 'Thanksgiving', 5, '2020-11-26', NULL, NULL, 1, 0, 0),
+(290, 'Thanksgiving', 6, '2020-11-26', NULL, NULL, 1, 0, 0),
+(291, 'Thanksgiving', 7, '2020-11-26', NULL, NULL, 1, 0, 0),
+(292, 'Thanksgiving', 8, '2020-11-26', NULL, NULL, 1, 0, 0),
+(293, 'Thanksgiving', 9, '2020-11-26', NULL, NULL, 1, 0, 0),
+(294, 'Day After Thanksgiving', 1, '2020-11-27', NULL, NULL, 1, 0, 0),
+(295, 'Day After Thanksgiving', 2, '2020-11-27', NULL, NULL, 1, 0, 0),
+(296, 'Day After Thanksgiving', 3, '2020-11-27', NULL, NULL, 1, 0, 0),
+(297, 'Day After Thanksgiving', 4, '2020-11-27', NULL, NULL, 1, 0, 0),
+(298, 'Day After Thanksgiving', 5, '2020-11-27', NULL, NULL, 1, 0, 0),
+(299, 'Day After Thanksgiving', 6, '2020-11-27', NULL, NULL, 1, 0, 0),
+(300, 'Day After Thanksgiving', 7, '2020-11-27', NULL, NULL, 1, 0, 0),
+(301, 'Day After Thanksgiving', 8, '2020-11-27', NULL, NULL, 1, 0, 0),
+(302, 'Day After Thanksgiving', 9, '2020-11-27', NULL, NULL, 1, 0, 0),
+(303, 'Christmas Eve', 1, '2020-12-24', NULL, NULL, 1, 0, 0),
+(304, 'Christmas Eve', 2, '2020-12-24', NULL, NULL, 1, 0, 0),
+(305, 'Christmas Eve', 3, '2020-12-24', NULL, NULL, 1, 0, 0),
+(306, 'Christmas Eve', 4, '2020-12-24', NULL, NULL, 1, 0, 0),
+(307, 'Christmas Eve', 5, '2020-12-24', NULL, NULL, 1, 0, 0),
+(308, 'Christmas Eve', 6, '2020-12-24', NULL, NULL, 1, 0, 0),
+(309, 'Christmas Eve', 7, '2020-12-24', NULL, NULL, 1, 0, 0),
+(310, 'Christmas Eve', 8, '2020-12-24', NULL, NULL, 1, 0, 0),
+(311, 'Christmas Eve', 9, '2020-12-24', NULL, NULL, 1, 0, 0),
+(312, 'Christmas', 1, '2020-12-25', NULL, NULL, 1, 0, 0),
+(313, 'Christmas', 2, '2020-12-25', NULL, NULL, 1, 0, 0),
+(314, 'Christmas', 3, '2020-12-25', NULL, NULL, 1, 0, 0),
+(315, 'Christmas', 4, '2020-12-25', NULL, NULL, 1, 0, 0),
+(316, 'Christmas', 5, '2020-12-25', NULL, NULL, 1, 0, 0),
+(317, 'Christmas', 6, '2020-12-25', NULL, NULL, 1, 0, 0),
+(318, 'Christmas', 7, '2020-12-25', NULL, NULL, 1, 0, 0),
+(319, 'Christmas', 8, '2020-12-25', NULL, NULL, 1, 0, 0),
+(320, 'Christmas', 9, '2020-12-25', NULL, NULL, 1, 0, 0),
+(321, 'Day after Christmas', 1, '2020-12-26', NULL, NULL, 1, 0, 0),
+(322, 'Day after Christmas', 2, '2020-12-26', NULL, NULL, 1, 0, 0),
+(323, 'Day after Christmas', 3, '2020-12-26', NULL, NULL, 1, 0, 0),
+(324, 'Day after Christmas', 4, '2020-12-26', NULL, NULL, 1, 0, 0),
+(325, 'Day after Christmas', 5, '2020-12-26', NULL, NULL, 1, 0, 0),
+(326, 'Day after Christmas', 6, '2020-12-26', NULL, NULL, 1, 0, 0),
+(327, 'Day after Christmas', 7, '2020-12-26', NULL, NULL, 1, 0, 0),
+(328, 'Day after Christmas', 8, '2020-12-26', NULL, NULL, 1, 0, 0),
+(329, 'Day after Christmas', 9, '2020-12-26', NULL, NULL, 1, 0, 0),
+(330, 'New Year\'s Day', 1, '2021-01-01', NULL, NULL, 1, 0, 0),
+(331, 'New Year\'s Day', 2, '2021-01-01', NULL, NULL, 1, 0, 0),
+(332, 'New Year\'s Day', 3, '2021-01-01', NULL, NULL, 1, 0, 0),
+(333, 'New Year\'s Day', 4, '2021-01-01', NULL, NULL, 1, 0, 0),
+(334, 'New Year\'s Day', 5, '2021-01-01', NULL, NULL, 1, 0, 0),
+(335, 'New Year\'s Day', 6, '2021-01-01', NULL, NULL, 1, 0, 0),
+(336, 'New Year\'s Day', 7, '2021-01-01', NULL, NULL, 1, 0, 0),
+(337, 'New Year\'s Day', 8, '2021-01-01', NULL, NULL, 1, 0, 0),
+(338, 'New Year\'s Day', 9, '2021-01-01', NULL, NULL, 1, 0, 0),
+(339, 'Martin Luther King, Jr. Day', 1, '2021-01-18', NULL, NULL, 1, 0, 0),
+(340, 'Martin Luther King, Jr. Day', 2, '2021-01-18', NULL, NULL, 1, 0, 0),
+(341, 'Martin Luther King, Jr. Day', 3, '2021-01-18', NULL, NULL, 1, 0, 0),
+(342, 'Martin Luther King, Jr. Day', 4, '2021-01-18', NULL, NULL, 1, 0, 0),
+(343, 'Martin Luther King, Jr. Day', 5, '2021-01-18', NULL, NULL, 1, 0, 0),
+(344, 'Martin Luther King, Jr. Day', 6, '2021-01-18', NULL, NULL, 1, 0, 0),
+(345, 'Martin Luther King, Jr. Day', 7, '2021-01-18', NULL, NULL, 1, 0, 0),
+(346, 'Martin Luther King, Jr. Day', 8, '2021-01-18', NULL, NULL, 1, 0, 0),
+(347, 'Martin Luther King, Jr. Day', 9, '2021-01-18', NULL, NULL, 1, 0, 0),
+(348, 'Presidents\' Day', 1, '2021-02-15', NULL, NULL, 1, 0, 0),
+(349, 'Presidents\' Day', 2, '2021-02-15', NULL, NULL, 1, 0, 0),
+(350, 'Presidents\' Day', 3, '2021-02-15', NULL, NULL, 1, 0, 0),
+(351, 'Presidents\' Day', 4, '2021-02-15', NULL, NULL, 1, 0, 0),
+(352, 'Presidents\' Day', 5, '2021-02-15', NULL, NULL, 1, 0, 0),
+(353, 'Presidents\' Day', 6, '2021-02-15', NULL, NULL, 1, 0, 0),
+(354, 'Presidents\' Day', 7, '2021-02-15', NULL, NULL, 1, 0, 0),
+(355, 'Presidents\' Day', 8, '2021-02-15', NULL, NULL, 1, 0, 0),
+(356, 'Presidents\' Day', 9, '2021-02-15', NULL, NULL, 1, 0, 0),
+(357, 'Easter', 1, '2021-04-04', NULL, NULL, 1, 0, 0),
+(358, 'Memorial Day', 1, '2021-05-31', NULL, NULL, 1, 0, 0),
+(359, 'Memorial Day', 2, '2021-05-31', NULL, NULL, 1, 0, 0),
+(360, 'Memorial Day', 3, '2021-05-31', NULL, NULL, 1, 0, 0),
+(361, 'Memorial Day', 4, '2021-05-31', NULL, NULL, 1, 0, 0),
+(362, 'Memorial Day', 5, '2021-05-31', NULL, NULL, 1, 0, 0),
+(363, 'Memorial Day', 6, '2021-05-31', NULL, NULL, 1, 0, 0),
+(364, 'Memorial Day', 7, '2021-05-31', NULL, NULL, 1, 0, 0),
+(365, 'Memorial Day', 8, '2021-05-31', NULL, NULL, 1, 0, 0),
+(366, 'Memorial Day', 9, '2021-05-31', NULL, NULL, 1, 0, 0),
+(367, 'Juneteenth', 1, '2021-06-19', NULL, NULL, 1, 0, 0),
+(368, 'Juneteenth', 2, '2021-06-19', NULL, NULL, 1, 0, 0),
+(369, 'Juneteenth', 3, '2021-06-19', NULL, NULL, 1, 0, 0),
+(370, 'Juneteenth', 4, '2021-06-19', NULL, NULL, 1, 0, 0),
+(371, 'Juneteenth', 5, '2021-06-19', NULL, NULL, 1, 0, 0),
+(372, 'Juneteenth', 6, '2021-06-19', NULL, NULL, 1, 0, 0),
+(373, 'Juneteenth', 7, '2021-06-19', NULL, NULL, 1, 0, 0),
+(374, 'Juneteenth', 8, '2021-06-19', NULL, NULL, 1, 0, 0),
+(375, 'Juneteenth', 9, '2021-06-19', NULL, NULL, 1, 0, 0);
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `Alert`
---
-ALTER TABLE `Alert`
-  ADD PRIMARY KEY (`AlertID`);
 
 --
 -- Indexes for table `Libraries`
@@ -554,12 +712,6 @@ ALTER TABLE `Special`
 --
 
 --
--- AUTO_INCREMENT for table `Alert`
---
-ALTER TABLE `Alert`
-  MODIFY `AlertID` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
 -- AUTO_INCREMENT for table `Libraries`
 --
 ALTER TABLE `Libraries`
@@ -569,25 +721,25 @@ ALTER TABLE `Libraries`
 -- AUTO_INCREMENT for table `Schedule`
 --
 ALTER TABLE `Schedule`
-  MODIFY `SchedID` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=267;
+  MODIFY `SchedID` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=323;
 
 --
 -- AUTO_INCREMENT for table `Season`
 --
 ALTER TABLE `Season`
-  MODIFY `SeasonID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `SeasonID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `Semesters`
 --
 ALTER TABLE `Semesters`
-  MODIFY `SemID` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `SemID` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `Special`
 --
 ALTER TABLE `Special`
-  MODIFY `SpecialID` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=247;
+  MODIFY `SpecialID` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=376;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

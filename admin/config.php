d2a1060602b166430e55b2942b81251f97a0c44c
Katherine Perdue <kperdue@jmrl.org>
2020-09-03 20:13:59 -0400

    fixing untracked files

--- admin/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('databasename', 'jmrl_hours'); // Name of the hours database
-define('databaseuser', 'hours'); // Username for database
-define('databasepassword', 'ywHAUJy8c283ADGTKWLW'); // Password for database
-define('databasehost', 'localhost'); // Normally should be localhost unless your database is on another server
-
-
-?>
